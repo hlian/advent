1cbd4dff516aebec9756eb6234c56edbe418091a
Hao Lian <hi@haolian.org>
2016-09-13 00:26:15 -0400

    22.hs fixed up

--- src/22.hs
+++ src/22.hs
@@ -1,42 +1,58 @@
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NoImplicitPrelude          #-}
 {-# LANGUAGE TemplateHaskell            #-}
 
 module Main where
 import BasePrelude hiding ((&), cast)
 import Control.Lens
 
+---------------
+--- some models
+
 data Spell =
   Msl | Drn | Shd | Psn | Rch
   deriving (Show, Eq, Enum, Bounded)
 
+-- | player health
 newtype HP = HP Int deriving (Num, Show, Eq, Ord)
+-- | boss health
 newtype BP = BP Int deriving (Num, Show, Eq, Ord)
+-- | player armor
 newtype Armor = Armor Int deriving (Num, Show, Eq, Ord)
+-- | player mana
 newtype Mana = Mana Int deriving (Num, Show, Eq, Ord)
 
 data Game =
-  Game { _hp :: HP, _bp :: BP, _armor ::  Armor, _mana :: Mana, _effs :: [(Spell, Int)], _spent :: Mana, _bd :: BP }
-  deriving (Show)
+  Game { _hp :: HP
+       , _bp :: BP
+       , _armor ::  Armor
+       , _mana :: Mana
+       , _effs :: [(Spell, Int)]
+       , _spent :: Mana
+       , _damage :: BP
+       } deriving Show
 
 makeLenses ''Game
 
+cast :: Spell -> Game -> Game
+cast Msl = (bp -~ 4) . (mana -~ 53) . (spent +~ 53)
+cast Drn = (bp -~ 2) . (hp +~ 2) . (mana -~ 73) . (spent +~ 73)
+cast Shd = (effs <>~ [(Shd, 6)]) . (mana -~ 113) . (spent +~ 113)
+cast Psn = (effs <>~ [(Psn, 6)]) . (mana -~ 173) . (spent +~ 173)
+cast Rch = (effs <>~ [(Rch, 5)]) . (mana -~ 229) . (spent +~ 229)
+
+-- | armor with shield
 armor' :: Getter Game Armor
 armor' =
   to (\game -> game ^. armor + (if active game Shd then 7 else 0))
 
-cast :: Spell -> Game -> Game
-cast spell game =
-  case spell of
-    Msl -> game & bp -~ 4 & mana -~ 53 & spent +~ 53
-    Drn -> game & bp -~ 2 & hp +~ 2 & mana -~ 73 & spent +~ 73
-    Shd -> game & effs <>~ [(Shd, 6)] & mana -~ 113 & spent +~ 113
-    Psn -> game & effs <>~ [(Psn, 6)] & mana -~ 173 & spent +~ 173
-    Rch -> game & effs <>~ [(Rch, 5)] & mana -~ 229 & spent +~ 229
-
+-- | returns True if the spell's effects are active (inside the effs
+-- set)
 active :: Game -> Spell -> Bool
 active game spell =
   spell `elem` game ^.. effs . traverse . _1
 
+runEffs :: Game -> Game
 runEffs =
   gc . erode . recharge . poison
   where
@@ -45,57 +61,97 @@ runEffs =
     erode game = game & effs . traverse . _2 -~ 1
     gc game = game & effs %~ filter ((/= 0) . snd)
 
-hpCheck game =
+------------------
+-- the game itself
+
+-- | dead players go to the Left and living players go to the Right
+-- | by throwing games into the Left bin, we short-circuit all the >=> chains below
+-- | (faster solution, easier-to-read output)
+hpGuard :: Game -> Either Game Game
+hpGuard game =
   if game ^. hp > 0 then Right game else Left game
+
+-- | dead bosses go to the Right and living bosses go the Left
+bpGuard :: Game -> Either Game Game
+bpGuard game =
+  if game ^. bp > 0 then Right game else Left game
 
 player :: Spell -> Game -> Either Game Game
 player spell =
-  manaCheck <=<
-  pure . cast spell <=<
-  pure . runEffs <=<
-  hpCheck <=<
-  pure . oof
+  pure . harden
+  >=> hpGuard
+  >=> pure . runEffs
+  >=> pure . cast spell
+  >=> manaCheck
   where
     manaCheck game =
       if game ^. mana > 0 then Right game else Left game
     -- set to `id` on easy mode
-    oof game =
+    harden game =
       game & hp -~ 1
 
 boss :: Game -> Either Game Game
 boss =
-  pure . runEffs >=>
-  hpCheck >=>
-  bpCheck >=>
-  pure . attack
+  pure . runEffs
+  >=> hpGuard
+  >=> bpGuard
+  >=> pure . attack
   where
    attack game =
      game & hp -~ damage' game
-    bpCheck game =
-      if game ^. bp > 0 then Right game else Left game
-    damage' game =
-      max 1 (case game ^. bd of BP i -> HP i - case game ^. armor' of Armor i -> HP i)
-
-step :: Spell -> Game -> Either Game Game
-step spell = player spell >=> boss
-
-comb :: Int -> [a] -> [[a]]
-comb 0 _ = [[]]
-comb i universe = do
-  x <- universe
-  (x:) <$> comb (i - 1) universe
+   -- player damage = max(1, boss damage - player armor with shields)
+   damage' game =
+     max 1 (HP (game ^. damage . coerced) - HP (game ^. armor' . coerced))
 
 space :: [[Spell]]
-space = concat [comb i xs | i <- [1..16]]
-  where xs = [(minBound :: Spell)..]
+space =
+  concat [comb i allSpells | i <- [1 .. n]]
+  where
+    allSpells :: [Spell]
+    allSpells = [minBound..]
 
+-- | for each spell, run player actions then boss actions
 play :: Game -> [Spell] -> Either Game Game
-play game0 spells = foldlM (\g s -> step s g) game0 spells
+play =
+  foldlM (\game spell -> (player spell >=> boss) game)
 
-valid game0 = [g | Left g <- play game0 <$> space, g ^. bp <= 0, g ^. hp > 0, g ^. mana >= 0]
+winning :: Game -> [Game]
+winning game0 =
+  [g | Left g <- play game0 <$> space -- give me all games that end after N turns
+     , g ^. bp <= 0 -- with a dead boss
+     , g ^. hp > 0 -- and a living player
+     , g ^. mana >= 0 -- and nonnegative mana
+     ]
 
--- for debugging
+main :: IO ()
+main = do
+  print "start with a low n"
+  print "pick an initial game"
+  print "run winning on an initial game"
+  print "if empty, increment and repeat"
 
+------------
+-- utilities
+
+-- | maximum number of steps to search for
+-- | the program is O(2^n)
+n :: Int
+n = 16
+
+-- | all combinations (without replacement) of a given set
+comb :: Int -> [a] -> [[a]]
+comb 0 _ = [[]]
+comb i set_ = do
+  x <- set_
+  (x:) <$> comb (i - 1) set_
+
+--------------------------------
+-- initial states (for GHCi use)
+
+start, start', start'' :: Game
+-- | the test one
 start = Game 50 51 0 500 [] 0 9
+-- | easy challenge
 start' = Game 10 14 0 250 [] 0 8
+-- | hard challenge
 start'' = Game 10 13 0 250 [] 0 8
