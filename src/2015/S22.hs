f8268d34f86a50da8f018211723881a52011b7f0
Hao Lian <hi@haolian.org>
2016-12-01 01:51:48 -0500

    2016

---
+++ src/2015/S22.hs
@@ -0,0 +1,155 @@
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE NoImplicitPrelude          #-}
+{-# LANGUAGE TemplateHaskell            #-}
+
+module S22 where
+
+import BasePrelude hiding ((&), cast)
+import Control.Lens
+
+---------------
+--- some models
+
+data Spell =
+  Msl | Drn | Shd | Psn | Rch
+  deriving (Show, Eq, Enum, Bounded)
+
+-- | player health
+newtype HP = HP Int deriving (Num, Show, Eq, Ord)
+-- | boss health
+newtype BP = BP Int deriving (Num, Show, Eq, Ord)
+-- | player armor
+newtype Armor = Armor Int deriving (Num, Show, Eq, Ord)
+-- | player mana
+newtype Mana = Mana Int deriving (Num, Show, Eq, Ord)
+
+data Game =
+  Game { _hp :: HP
+       , _bp :: BP
+       , _armor ::  Armor
+       , _mana :: Mana
+       , _effs :: [(Spell, Int)]
+       , _spent :: Mana
+       , _damage :: BP
+       } deriving Show
+
+makeLenses ''Game
+
+cast :: Spell -> Game -> Game
+cast Msl = (bp -~ 4) . (mana -~ 53) . (spent +~ 53)
+cast Drn = (bp -~ 2) . (hp +~ 2) . (mana -~ 73) . (spent +~ 73)
+cast Shd = (effs <>~ [(Shd, 6)]) . (mana -~ 113) . (spent +~ 113)
+cast Psn = (effs <>~ [(Psn, 6)]) . (mana -~ 173) . (spent +~ 173)
+cast Rch = (effs <>~ [(Rch, 5)]) . (mana -~ 229) . (spent +~ 229)
+
+-- | armor with shield
+armor' :: Getter Game Armor
+armor' =
+  to (\game -> game ^. armor + (if active game Shd then 7 else 0))
+
+-- | returns True if the spell's effects are active (inside the effs
+-- set)
+active :: Game -> Spell -> Bool
+active game spell =
+  spell `elem` game ^.. effs . traverse . _1
+
+runEffs :: Game -> Game
+runEffs =
+  gc . erode . recharge . poison
+  where
+    poison game = game & bp -~ if active game Psn then 3 else 0
+    recharge game = game & mana +~ if active game Rch then 101 else 0
+    erode game = game & effs . traverse . _2 -~ 1
+    gc game = game & effs %~ filter ((/= 0) . snd)
+
+------------------
+-- the game itself
+
+-- | dead players go to the Left and living players go to the Right
+-- | by throwing games into the Left bin, we short-circuit all the >=> chains below
+-- | (faster solution, easier-to-read output)
+hpGuard :: Game -> Either Game Game
+hpGuard game =
+  if game ^. hp > 0 then Right game else Left game
+
+-- | dead bosses go to the Right and living bosses go the Left
+bpGuard :: Game -> Either Game Game
+bpGuard game =
+  if game ^. bp > 0 then Right game else Left game
+
+player :: Spell -> Game -> Either Game Game
+player spell =
+  pure . harden >=> hpGuard >=> pure . runEffs >=> pure . cast spell >=> manaCheck
+  where
+    manaCheck game =
+      if game ^. mana > 0 then Right game else Left game
+    harden =
+      if hard then hp -~ 1 else id
+
+boss :: Game -> Either Game Game
+boss =
+  pure . runEffs >=> hpGuard >=> bpGuard >=> pure . attack
+  where
+   attack game =
+     game & hp -~ damage' game
+   -- player damage = max(1, boss damage - player armor with shields)
+   damage' game =
+     max 1 (HP (game ^. damage . coerced) - HP (game ^. armor' . coerced))
+
+space :: [[Spell]]
+space =
+  concat [comb i allSpells | i <- [1 .. n]]
+  where
+    allSpells :: [Spell]
+    allSpells = [minBound..]
+
+-- | for each spell, run player actions then boss actions
+play :: Game -> [Spell] -> Either Game Game
+play =
+  foldlM (\game spell -> (player spell >=> boss) game)
+
+winning :: Game -> [Game]
+winning game0 =
+  [g | Left g <- play game0 <$> space -- give me all games that end after N turns
+     , g ^. bp <= 0 -- with a dead boss
+     , g ^. hp > 0 -- and a living player
+     , g ^. mana >= 0 -- and nonnegative mana
+     ]
+
+main :: IO ()
+main = do
+  print "start with a low n"
+  print "pick an initial game"
+  print "run winning on an initial game"
+  print "if empty, increment and repeat"
+
+------------
+-- utilities
+
+-- | all combinations (without replacement) of a given set
+comb :: Int -> [a] -> [[a]]
+comb 0 _ = [[]]
+comb i set_ = do
+  x <- set_
+  (x:) <$> comb (i - 1) set_
+
+--------------------------------
+-- initial states (for GHCi use)
+
+start, start', start'' :: Game
+-- | the game input (winnable in n = 16 on hard)
+start = Game 50 51 0 500 [] 0 9
+-- | an example from the problem (winnable in n = 5 on easy)
+start' = Game 10 14 0 250 [] 0 8
+-- | a second example from the problem (winnable in n = 2 on easy)
+start'' = Game 10 13 0 250 [] 0 8
+
+-- | maximum number of steps to search for
+-- | the program is O(2^n)
+n :: Int
+n = 5
+
+-- | nnh
+hard :: Bool
+hard = False
+
---
+++ src/2016/D2.hs
@@ -0,0 +1,45 @@
+#!/usr/bin/env stack
+-- stack --resolver lts-6.26 --install-ghc runghc --package text --package base-prelude
+
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE ViewPatterns      #-}
+
+import qualified Data.Text as Text
+import BasePrelude
+
+data Direction =
+  R | L
+input =
+  "R3, L2, L2, R4, L1, R2, R3, R4, L2, R4, L2, L5, L1, R5, R2, R2, L1, R4, R1, L5, L3, R4, R3, R1, L1, L5, L4, L2, R5, L3, L4, R3, R1, L3, R1, L3, R3, L4, R2, R5, L190, R2, L3, R47, R4, L3, R78, L1, R3, R190, R4, L3, R4, R2, R5, R3, R4, R3, L1, L4, R3, L4, R1, L4, L5, R3, L3, L4, R1, R2, L4, L3, R3, R3, L2, L5, R1, L4, L1, R5, L5, R1, R5, L4, R2, L2, R1, L5, L4, R4, R4, R3, R2, R3, L1, R4, R5, L2, L5, L4, L1, R4, L4, R4, L4, R1, R5, L1, R1, L5, R5, R1, R1, L3, L1, R4, L1, L4, L4, L3, R1, R4, R1, R1, R2, L5, L2, R4, L1, R3, L5, L2, R5, L4, R5, L5, R3, R4, L3, L3, L2, R2, L5, L5, R3, R4, R3, R4, R3, R1"
+steps =
+  [case direction of
+     'R' -> (R, read rest :: Int)
+     'L' -> (L, read rest :: Int)
+  | direction:rest <- map Text.unpack (Text.splitOn ", " input)]
+turn compass dir =
+  mod (compass + case dir of R -> 1; L -> -1) 4
+solution1 =
+  join $
+    scanl (\before (direction, n) ->
+             case last before of
+               (compass, x, y) -> case turn compass direction of
+                 0 -> [(0, x, y + i) | i <- [0 .. n]]
+                 1 -> [(1, x + i, y) | i <- [0 .. n]]
+                 2 -> [(2, x, y - i) | i <- [0 .. n]]
+                 3 -> [(3, x - i, y) | i <- [0 .. n]]
+          ) [(0, 0, 0)] steps
+solution2 =
+  duplicates . map positionOf $ solution1
+positionOf (_, x, y) =
+  (x, y)
+duplicates hay =
+  hay !! fst (head dups)
+  where
+    dups = [(e, ix) | (e, ix) <- zip [0..] ixs, ix /= e, ix + 1 /= e]
+    ixs  = [i | needle <- hay, Just i <- [elemIndex needle hay]]
+main = do
+  print (last solution1)
+  case last solution1 of (_, x, y) -> print $ x + y
+  print solution2
+  case solution2 of (x, y) -> print $ x + y
