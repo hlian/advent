cdcdc4b18fcda2d780334e724bbfe3598da44c5c
Hao Lian <hi@haolian.org>
2016-12-23 06:21:14 +0000

    D23 was fun

---
+++ src/2016/D23Input.hs
@@ -0,0 +1,39 @@
+{-# LANGUAGE OverloadedLists #-}
+
+module D23Input where
+
+import           Data.Vector (Vector)
+import qualified Data.Vector as Vector
+
+data Param = R Char | I Int deriving (Show, Eq)
+data Op = CPY Param Param | DEC Param | INC Param | JNZ Param Param | TGL Param | NOP | PLUS Param Param | MULT Param Param Param deriving (Show, Eq)
+
+input :: Vector Op
+input = [
+    CPY (R 'a') (R 'b'),
+    DEC (R 'b'),
+    CPY (R 'a') (R 'd'),
+    CPY (I 0) (R 'a'),
+    MULT (R 'a') (R 'b') (R 'd'),
+    CPY (I 0) (R 'c'),
+    CPY (I 0) (R 'd'),
+    NOP,
+    NOP,
+    NOP,
+    DEC (R 'b'),
+    CPY (R 'b') (R 'c'),
+    CPY (R 'c') (R 'd'),
+    DEC (R 'd'),
+    INC (R 'c'),
+    JNZ (R 'd') (I(-2)),
+    TGL (R 'c'),
+    CPY (I(-16)) (R 'c'),
+    JNZ (I 1) (R 'c'),
+    CPY (I 81) (R 'c'),
+    JNZ (I 73) (R 'd'),
+    INC (R 'a'),
+    INC (R 'd'),
+    JNZ (R 'd') (I(-2)),
+    INC (R 'c'),
+    JNZ (R 'c') (I(-5))
+  ]
--- src/2016/Main.hs
+++ src/2016/Main.hs
@@ -1,54 +1,69 @@
 #!/usr/bin/env stack
--- stack --resolver lts-7.14 --install-ghc runghc --package lens --package base-prelude
+-- stack runghc
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE ViewPatterns      #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE GADTs #-}
 module Main where
-import           BasePrelude hiding ((&), left)
+
+import           BasePrelude hiding ((&), left, lookup, loop)
 import           Control.Lens
-import           D20Input
 import           Data.Map (Map)
 import qualified Data.Map as Map
 import qualified Data.Sequence as Seq
-import           Data.Set (Set)
-import qualified Data.Set as Set
+import           Data.Vector (Vector)
+import qualified Data.Vector as Vector
 
+import           Data.Hashable
 
-solution input =
-  ""
+import           AStar
+import           D23Input
 
-windows row =
-  filter ((== 2) . length) $ map (take 2) (tails row)
+type State = (Int, Map Char Int, Vector Op)
+empty0 :: Int -> State
+empty0 i = (0, Map.empty & at 'a' ?~ i, input)
 
-example =
-  [(-1, -1), (11, 11)]
+lookup :: Param -> State -> Int
+lookup (R r) st =  st ^. _2 . at r . non 0
+lookup (I i) _ = i
 
-ok [(a, b), (c, d)] =
-  b + 1 < c
+interp :: Op -> State -> State
+interp (CPY src (R r)) st =
+  st & _2 . at r ?~ lookup src st
+     & _1 +~ 1
+interp (INC (R r)) st =
+  st & _2 . at r . _Just +~ 1
+     & _1 +~ 1
+interp (DEC (R r)) st =
+  st & _2 . at r . _Just -~ 1
+     & _1 +~ 1
+interp (JNZ nonzero away) st = do
+  let nonzero0 = lookup nonzero st
+  let away0 = lookup away st
+  st & _1 %~ (\x -> x + if nonzero0 == 0 then 1 else away0)
+interp (PLUS (R a) (R b)) st =
+  st & _2 . at a . _Just +~ lookup (R b) st
+     & _1 +~ 1
+interp NOP st =
+  st & _1 +~ 1
+interp (MULT (R a) (R b) (R c)) st =
+  st & _1 +~ 1
+     & _2 . at a . _Just .~ lookup (R b) st * lookup (R c) st
+interp (TGL (R c)) st =
+  st & _1 +~ 1
+     & _3 . ix (st ^. _1 + lookup (R c) st) %~ toggle
 
-overlap (a, b) (c, d) =
-  b >= c - 1 && a <= d
+toggle :: Op -> Op
+toggle (CPY a b) = (JNZ a b)
+toggle (TGL a) = INC a
+toggle (DEC a) = INC a
+toggle (INC a) = DEC a
+toggle (JNZ a b) = CPY a b
 
-merge whole@((a, b):(c, d):rest) =
-  if overlap (a, b) (c, d) then merge ((a,max b d):rest) else ((a,b):merge ((c,d):rest))
-merge xs | length xs <= 1 = xs
-
-diff [(a, b), (c, d)] =
-  if c >= b + 1 then ((c - 1) - (b + 1) + 1) else error "whoa"
-
-f (a, b) (l, h, acc) | a > h + 1 = (a, b, (l,h):acc)
-                     | overlap (a, b) (l, h) = (l, b, acc)
-                     | otherwise = (a, max b h, acc)
-
-solve2 _ n [] = n
-solve2 blocked n ((low,hi):rest)
-    | low > blocked+1 = solve2 (max blocked hi) (n+low-blocked-1) rest
-    | otherwise       = solve2 (max blocked hi) n rest
+loop st@(i, env, cmds)
+  | (Vector.null cmds || i >= length cmds) = st
+  | otherwise = loop (interp (cmds ^?! ix i) st)
 
 main = do
-  -- print (sort example)
-  -- print (filter ok $ windows (sort example))
-  -- print (filter ok $ windows (sort input))
-  let n = 2200000000000000
-  -- print (take n $ sort input)
-  print (sum $ map diff . windows . merge $ take n $ sort input)
-  -- print (4294967296 :: Integer)
+  print (loop (empty0 7))
+  print (loop (empty0 12))
