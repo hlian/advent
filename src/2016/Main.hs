36cc76815d7df481105ea8db57fe69b36eba6526
Hao Lian <hi@haolian.org>
2016-12-24 06:15:14 +0000

    d24

--- src/2016/Main.hs
+++ src/2016/Main.hs
@@ -1,69 +1,45 @@
-#!/usr/bin/env stack
--- stack runghc
 {-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE ViewPatterns      #-}
-{-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE GADTs #-}
 module Main where
 
-import           BasePrelude hiding ((&), left, lookup, loop)
+import           BasePrelude hiding ((&))
 import           Control.Lens
-import           Data.Map (Map)
+import           D24Input
 import qualified Data.Map as Map
-import qualified Data.Sequence as Seq
-import           Data.Vector (Vector)
-import qualified Data.Vector as Vector
+import qualified Data.Set as Set
 
-import           Data.Hashable
+graph input =
+  Map.fromList [((x, y), c) | (y, s) <- zip [0..] input, (x, c) <- zip [0..] s]
 
-import           AStar
-import           D23Input
+wire needle graph =
+  head [(needle, x, y) | ((x, y), c) <- Map.toList graph, c == needle]
 
-type State = (Int, Map Char Int, Vector Op)
-empty0 :: Int -> State
-empty0 i = (0, Map.empty & at 'a' ?~ i, input)
+neighbors g (_, x, y) = concat . map try $ [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]
+  where try (x, y) = case g ^. at (x, y) of
+          Just c | c /= '#' -> [(c, x, y)]
+          _ -> []
 
-lookup :: Param -> State -> Int
-lookup (R r) st =  st ^. _2 . at r . non 0
-lookup (I i) _ = i
+dist init goal neighbors = rec (Set.singleton init) 0
+  where rec frontier steps = do
+          let ok = filter (== goal) (Set.toList frontier)
+          if Set.null frontier then error "invalid goal story"
+          else if length ok > 0 then steps
+          else rec (Set.fromList . concatMap neighbors . Set.toList $ frontier) (steps + 1)
 
-interp :: Op -> State -> State
-interp (CPY src (R r)) st =
-  st & _2 . at r ?~ lookup src st
-     & _1 +~ 1
-interp (INC (R r)) st =
-  st & _2 . at r . _Just +~ 1
-     & _1 +~ 1
-interp (DEC (R r)) st =
-  st & _2 . at r . _Just -~ 1
-     & _1 +~ 1
-interp (JNZ nonzero away) st = do
-  let nonzero0 = lookup nonzero st
-  let away0 = lookup away st
-  st & _1 %~ (\x -> x + if nonzero0 == 0 then 1 else away0)
-interp (PLUS (R a) (R b)) st =
-  st & _2 . at a . _Just +~ lookup (R b) st
-     & _1 +~ 1
-interp NOP st =
-  st & _1 +~ 1
-interp (MULT (R a) (R b) (R c)) st =
-  st & _1 +~ 1
-     & _2 . at a . _Just .~ lookup (R b) st * lookup (R c) st
-interp (TGL (R c)) st =
-  st & _1 +~ 1
-     & _3 . ix (st ^. _1 + lookup (R c) st) %~ toggle
 
-toggle :: Op -> Op
-toggle (CPY a b) = (JNZ a b)
-toggle (TGL a) = INC a
-toggle (DEC a) = INC a
-toggle (INC a) = DEC a
-toggle (JNZ a b) = CPY a b
+measure dists  =
+  foldl (\(last, acc) next -> (next, (find (last, next):acc))) ('0', [])
+  where
+    find (x, y) = fromJust $ dists ^. at (x, y) <|> dists ^. at (y, x)
 
-loop st@(i, env, cmds)
-  | (Vector.null cmds || i >= length cmds) = st
-  | otherwise = loop (interp (cmds ^?! ix i) st)
+comb 0 _ = [[]]
+comb _ [] = []
+comb m (x:xs) = map (x:) (comb (m-1) xs) ++ comb m xs
 
 main = do
-  print (loop (empty0 7))
-  print (loop (empty0 12))
+  print . minimum $ map (sum . snd . measure dists) (permutations . drop 1 $ numbers)
+  print . minimum $ map (sum . snd . measure dists . (<> "0")) (permutations . drop 1 $ numbers)
+  where
+    (numbers, g) = ("01234567", graph input)
+    dists = Map.fromList $ [((src, dst), dist (wire src g) (wire dst g) (neighbors g))
+                           | [src, dst] <- comb 2 numbers]
